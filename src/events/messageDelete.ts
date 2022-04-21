fd031fd58aa41a8af5a100a4376d4019f7afc1f3
Yun Do Hyun <yundohyun050121@gmail.com>
2022-04-21 13:14:36 +0900

    add: messageDelete Event

---
+++ src/events/messageDelete.ts
@@ -0,0 +1,17 @@
+import { Message, TextChannel } from "discord.js";
+import { getCustomRepository } from "typeorm";
+import { MessageService } from "../services/message";
+import { Event } from "../types/event";
+
+const messageService = getCustomRepository(MessageService);
+
+export default new Event('messageDelete', async message =>{
+    message = message as Message;
+    if(!message.author.bot) {
+        if(message.channel as TextChannel && message) {
+            const body = await messageService.getByMessage(message);
+            body.deleted = +new Date();
+            messageService.updateByMessage(message,body);
+        }
+    }
+})
