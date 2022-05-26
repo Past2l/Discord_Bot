aba47a0a3c8bb44941687c3aa95e4fd7f33571c1
Yun Do Hyeon <yundohyun050121@gmail.com>
2022-05-26 22:21:19 +0900

    edit: detect empty message and attachment

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -1,47 +1,46 @@
 import { Message, TextChannel } from "discord.js";
-import { getCustomRepository } from "typeorm";
-import { ChannelEntity } from "../entities/Channel";
-import { GuildEntity } from "../entities/Guild";
-import { UserEntity } from "../entities/User";
-import { ChannelService } from "../services/channel";
-import { GuildService } from "../services/guild";
-import { MessageService } from "../services/message";
-import { UserService } from "../services/user";
 import { Event } from "../types/event";
+import { getCustomRepository } from "typeorm";
+import { GuildEntity } from "../entities/Guild";
+import { ChannelEntity } from "../entities/Channel";
+import { UserEntity } from "../entities/User";
+import { UserService } from "../services/user";
+import { GuildService } from "../services/guild";
+import { ChannelService } from "../services/channel";
+import { MessageService } from "../services/message";
 
-export default new Event('messageDelete', async message =>{
-    if(process.env.BOT_LOG==='true') {
+export default new Event('messageDelete', async message => {
+    if(process.env.BOT_LOG=='true') {
         const guildService = getCustomRepository(GuildService);
         const channelService = getCustomRepository(ChannelService);
         const userService = getCustomRepository(UserService);
         const messageService = getCustomRepository(MessageService);
         message = message as Message;
-        if(!message.author.bot && !message.author.system) {
-            if(message.channel instanceof TextChannel && message) {
-                const guild: GuildEntity = await guildService.get(message.guildId!) || await guildService.writeByGuild(message.guild!);
-                const channel: ChannelEntity = await channelService.get(message.channelId) || await channelService.writeByChannel({
+        if(!message.author.bot && !message.author.system && message.channel instanceof TextChannel && message) {
+            if(message.content.length <= 0 && message.attachments.size <= 0) return;
+            const guild: GuildEntity = await guildService.get(message.guildId!) || await guildService.writeByGuild(message.guild!);
+            const channel: ChannelEntity = await channelService.get(message.channelId) || await channelService.writeByChannel({
+                guild: guild,
+                channel: message.channel
+            });
+            const user: UserEntity = await userService.get(message.author.id) || await userService.writeByUser(message.author);
+            const body = await messageService.getByMessage(message);
+            if(body) {
+                body.deleted = +new Date();
+                const res = await messageService.get(message.id);
+                await messageService.updateByMessage({
+                    guild: guild,
+                    channel: channel,
+                    user: user,
+                    message: res!,
+                    content: message
+                }, body);
+                await guildService.updateByGuild(message.guild!);
+                await channelService.updateByChannel({
                     guild: guild,
                     channel: message.channel
                 });
-                const user: UserEntity = await userService.get(message.author.id) || await userService.writeByUser(message.author);
-                const body = await messageService.getByMessage(message);
-                if(body) {
-                    body.deleted = +new Date();
-                    const res = await messageService.get(message.id);
-                    await messageService.updateByMessage({
-                        guild: guild,
-                        channel: channel,
-                        user: user,
-                        message: res!,
-                        content: message
-                    }, body);
-                    await guildService.updateByGuild(message.guild!);
-                    await channelService.updateByChannel({
-                        guild: guild,
-                        channel: message.channel
-                    });
-                    await userService.updateByUser(message.author);
-                }
+                await userService.updateByUser(message.author);
             }
         }
     }
--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,47 +1,46 @@
-import { Event } from "../types/event";
 import { Message, TextChannel } from "discord.js";
+import { Event } from "../types/event";
 import { getCustomRepository } from "typeorm";
-import { MessageService } from "../services/message";
-import { ChannelService } from "../services/channel";
-import { UserService } from "../services/user";
-import { GuildService } from "../services/guild";
+import { GuildEntity } from "../entities/Guild";
 import { ChannelEntity } from "../entities/Channel";
 import { UserEntity } from "../entities/User";
-import { GuildEntity } from "../entities/Guild";
+import { UserService } from "../services/user";
+import { GuildService } from "../services/guild";
+import { ChannelService } from "../services/channel";
+import { MessageService } from "../services/message";
 
 export default new Event('messageUpdate',async (oldMessage, newMessage) => {
-    if(process.env.BOT_LOG==='true') {
+    if(process.env.BOT_LOG=='true') {
         const guildService = getCustomRepository(GuildService);
         const channelService = getCustomRepository(ChannelService);
         const userService = getCustomRepository(UserService);
         const messageService = getCustomRepository(MessageService);
         const message = newMessage as Message;
-        if(!message.author.bot && !message.author.system) {
-            if(message.channel instanceof TextChannel && message) {
-                const guild: GuildEntity = await guildService.get(message.guildId!) || await guildService.writeByGuild(message.guild!);
-                const channel: ChannelEntity = await channelService.get(message.channelId) || await channelService.writeByChannel({
+        if(!message.author.bot && !message.author.system && message.channel instanceof TextChannel) {
+            if(message.content.length <= 0 && message.attachments.size <= 0) return;
+            const guild: GuildEntity = await guildService.get(message.guildId!) || await guildService.writeByGuild(message.guild!);
+            const channel: ChannelEntity = await channelService.get(message.channelId) || await channelService.writeByChannel({
+                guild: guild,
+                channel: message.channel
+            });
+            const user: UserEntity = await userService.get(message.author.id) || await userService.writeByUser(message.author);
+            const body = await messageService.getByMessage(message);
+            if(body) {
+                body.edited = message.editedTimestamp || message.createdTimestamp;
+                const res = await messageService.get(message.id);
+                await messageService.updateByMessage({
+                    guild: guild,
+                    channel: channel,
+                    user: user,
+                    message: res!,
+                    content: message
+                }, body);
+                await guildService.updateByGuild(message.guild!);
+                await channelService.updateByChannel({
                     guild: guild,
                     channel: message.channel
                 });
-                const user: UserEntity = await userService.get(message.author.id) || await userService.writeByUser(message.author);
-                const body = await messageService.getByMessage(message);
-                if(body) {
-                    body.edited = message.editedTimestamp || message.createdTimestamp;
-                    const res = await messageService.get(message.id);
-                    await messageService.updateByMessage({
-                        guild: guild,
-                        channel: channel,
-                        user: user,
-                        message: res!,
-                        content: message
-                    }, body);
-                    await guildService.updateByGuild(message.guild!);
-                    await channelService.updateByChannel({
-                        guild: guild,
-                        channel: message.channel
-                    });
-                    await userService.updateByUser(message.author);
-                }
+                await userService.updateByUser(message.author);
             }
         }
     }
